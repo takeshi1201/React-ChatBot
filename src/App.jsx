c2c5d41bb63fefb0c53e88f6ece4a69cc7e018a4
takeshi1201 <kodama19961201@gmail.com>
2021-03-05 16:50:20 +0900

    Add:Answer、AnswerListコンポーネント追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import defaultDataset from "./dataset";
 import "./assets/styles/style.css";
+import {AnswersList} from './components/index';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,14 +13,25 @@ export default class App extends React.Component {
         dataset: defaultDataset,
         open: false,
       }
-    
+  }
+
+  initAnswers = () => {
+    const initDataset = this.state.dataset[this.state.currentId];
+    const initAnswers = initDataset.answers;
+    this.setState( {
+      answers: initAnswers
+    });
+  }
+
+  componentDidMount() {
+    this.initAnswers();
   }
 
   render () {
     return (
       <section className="c-section">
         <div className="c-box">
-
+          <AnswersList answers={this.state.answers} />
         </div>
       </section>
     );
