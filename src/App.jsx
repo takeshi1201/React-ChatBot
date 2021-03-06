b967260d41b947d0b59386ad3a69443a844951d8
takeshi1201 <kodama19961201@gmail.com>
2021-03-06 18:10:02 +0900

    Add:：FormDialogコンポーネント追加、Slackでお問い合わせ受信

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import defaultDataset from "./dataset";
 import "./assets/styles/style.css";
 import {AnswersList, Chats} from './components/index';
+import FormDialog from './components/Forms/FormDialog';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,6 +15,9 @@ export default class App extends React.Component {
         open: false,
       }
       this.selectAnswer = this.selectAnswer.bind(this);
+      this.handleClickOpen = this.handleClickOpen.bind(this);
+      this.handleClose = this.handleClose.bind(this);
+      
   }
 
   displayNextQuestion = (nextQuestionId) => {
@@ -34,6 +38,10 @@ export default class App extends React.Component {
     switch (true) {
       case (nextQuestionId === 'init'):
         setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
+        break;
+
+      case (nextQuestionId === 'contact'):
+        this.handleClickOpen();
         break;
       case(/^https:*/.test(nextQuestionId)):
         const a =document.createElement('a');
@@ -56,6 +64,13 @@ export default class App extends React.Component {
         break;
     }
   }
+  handleClickOpen () {
+    this.setState( { open: true } );
+  };
+
+  handleClose () {
+    this.setState( { open: false } );
+  };
 
   componentDidMount() {
     const initAnswers = " ";
@@ -75,6 +90,7 @@ export default class App extends React.Component {
         <div className="c-box">
           <Chats chats={this.state.chats} />
           <AnswersList answers={this.state.answers} select={this.selectAnswer} />
+          <FormDialog open={this.state.open} handleClose={this.handleClose} />
         </div>
       </section>
     );
